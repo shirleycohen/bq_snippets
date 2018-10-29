0e9160c958c7c6d53f4826da9210bdae6948d4a7
Shirley Cohen <scohen@cs.utexas.edu>
2018-10-29 16:33:18 -0500

    added example to copy yesterday's table

--- scheduled_query.sql
+++ scheduled_query.sql
@@ -1,4 +1,4 @@
-# Example that copies yesterday's firebase table to another table in BQ via a Scheduled Query in BigQuery
+# Copies yesterday's firebase table every 24 hours to another table in BQ via a Scheduled Query
 # 
 # Scheduled Query parameters:
 # Destination: <dataset_destination>
@@ -15,6 +15,24 @@ select *
 from `scohen-firebase-sandbox.analytics_153293282.events_*`
 where _TABLE_SUFFIX = replace(cast(DATE_SUB(@run_date, INTERVAL 1 DAY) as STRING), '-', '')
 
+
+# Copies today's firebase table every 24 hours to another table in BQ via a Scheduled Query
+# 
+# Scheduled Query parameters:
+# Destination: <dataset_destination>
+# Query string: see below
+# Destination table: events_{run_date}
+# Write preference: WRITE_TRUNCATE
+# Partitioning field: N/A
+# Next import run: N/A
+# Schedule: every day at 15:05
+# Email notifications: On transfer failures
+
+# Example query string:
+select * 
+from `scohen-firebase-sandbox.analytics_153293282.events_*`
+where _TABLE_SUFFIX = replace(cast(@run_date as STRING), '-', '')
+
 # Additional notes: 
-# before scheduling query, display destination project as the default project in BigQuery legacy UI
-# after scheduling query, use manual runs option to backfill previous dates (e.g. 2018-10-20 - 2018-10-24)
+# before scheduling the query, display destination project as the default project in BigQuery legacy UI
+# after scheduling query, use manual runs to backfill (e.g. 2018-10-20 - 2018-10-25)
